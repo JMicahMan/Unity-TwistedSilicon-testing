674b52ff0e5b695cbd28b30b951285e61cb5be8a
JMicahMan <j.micah.manzanares@gmail.com>
2019-06-24 17:55:33 -0500

    Waypoints work

--- Assets/Scripts/BadiCarBehaviour.cs
+++ Assets/Scripts/BadiCarBehaviour.cs
@@ -1,57 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BadiCarBehaviour : MonoBehaviour {
 
-    public GameObject waypoint1;
-    public GameObject waypoint2;
-    public GameObject waypoint3;
-    public GameObject waypoint4;
-    public GameObject waypoint5;
-    public GameObject waypoint6;
-    public GameObject waypoint7;
-    public GameObject waypoint8;
-    public GameObject waypoint9;
-    public GameObject waypoint10;
-    public GameObject waypoint11;
-    public GameObject waypoint12;
-    public GameObject waypoint13;
-    public GameObject waypoint14;
-    public GameObject waypoint15;
-    public GameObject waypoint16;
-    public GameObject waypoint17;
-    public GameObject waypoint18;
-    public GameObject waypoint19;
-    public GameObject waypoint20;
-    public GameObject waypoint21;
-    public GameObject waypoint22;
-    public GameObject waypoint23;
-    public GameObject waypoint24;
-    public GameObject waypoint25;
+    public GameObject[] Waypoint;
+    private int curntpoint = 0;
+    private NavMeshAgent agent;
 
     void Start ()
     {
-		
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+        //doing this keeps it seek instead of arrive^
+
+
+        NextPoint();
 	}
-	
-	void Update ()
+
+    void NextPoint()
     {
-        transform.position = waypoint1.transform.position;
 
-        if (transform.position == waypoint1.transform.position)
+        if (Waypoint.Length == 0)
         {
-            transform.position = waypoint2.transform.position;
-            if (transform.position == waypoint2.transform.position)
-            {
-                transform.position = waypoint2.transform.position;
-                if (transform.position == waypoint1.transform.position)
-                {
-                    transform.position = waypoint2.transform.position;
-                }
+            return;
+        }
+        //incase No points are set up^
 
-            }
+        agent.destination = Waypoint[curntpoint].transform.position;
 
+        curntpoint = (curntpoint + 1) % Waypoint.Length;
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 50)
+        {
+            NextPoint();
         }
     }
 }
